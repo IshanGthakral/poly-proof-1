aca996e439b3979f8e5e086953b0fef564285914
IshanGthakral <129625994+IshanGthakral@users.noreply.github.com>
2023-07-26 09:50:25 +0530

    Add files via upload

---
+++ metsdata/contractAddress.js
@@ -0,0 +1,3 @@
+
+    export const nftAddress = "0x711744C9D3Cd52737C09C71F549b93F5DadDa33c"
+  
---
+++ metsdata/nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+  {
+    name: "0",
+    description: "dog 1",
+    image: "QmbJ4vBouQ4NDSPCxbpNCh9roZvbK5Qr1pS5vPrZHUvKwk",
+  },
+  {
+    name: "1",
+    description: "labra",
+    image: "QmYHUptBFLJbDjRBJxs3nsPtqYa3CSUNh3w8BeUTEMjFoQ",
+  }, //change
+  {
+    name: "2",
+    description: "wolf",
+    image: "QmRuS3VFpQAvMS2NHuSzvkicxfjff4p5EN1NbmGnvpw1VR",
+  },
+  {
+    name: "3",
+    description: "pug",
+    image: "QmXPMEotvT6LZCnr3GBeSJbPt7cu3g8UoroVNXdYzwQnP5",
+  },
+  {
+    name: "4",
+    description: "dogmine.",
+    image: "QmWjnGMxztXSkHKLjmTRuwvoUrZ2SGLTrDQLnvdUDGdJ44",
+  },
+];
+
+module.exports = nfts;
